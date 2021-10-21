8c12a151dc546b8b474d89bc57d833ce2a9c786c
GinoGreen <luigiverde97@gmail.com>
2021-10-21 17:03:09 +0200

    doppio sconto minorenne + codice sconto

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ L’output del prezzo finale va messo fuori in forma umana (con massimo due deci
 //Dichiarazione variabili
 const kiloMeters = parseFloat(prompt('Inserisci i kilometri da percorrere'));
 const age = parseInt(prompt('Inserisci la tua etá'));
+const codeDiscount = prompt('Inserisci il codice sconto, se ne hai uno');
 const pricePerKm = 0.21;
 let priceTotal = pricePerKm * kiloMeters;
 let toSubtract = 0;
@@ -17,16 +18,35 @@ let ageMinMaj = 'maggiorenne under 65';
 let discountUsed = 0;
 
 //Inizio esercizio
+
+
 if (age < 18) {
    ageMinMaj = 'minorenne';
-   toSubtract = (priceTotal / 100) * discount20;
    discountUsed = discount20;
+
+   if (codeDiscount === 'SCONTO20') {
+      toSubtract = ((priceTotal / 100) * discount20) * 2 ;
+      document.getElementById('double-discount').innerHTML = 
+      `
+         Il tuo codice sconto é stato applicato correttamente, hai ricevuto un doppio sconto del 20 %
+      `;
+   } else {
+      toSubtract = (priceTotal / 100) * discount20;
+   }
+   
+} else if (age <= 20 && codeDiscount === 'SCONTO20') {
+   toSubtract = ((priceTotal / 100) * discount20) ;
+   document.getElementById('double-discount').innerHTML = 
+      `
+         Il tuo codice sconto é stato applicato correttamente, hai ricevuto uno sconto del 20 %
+      `;
 } else if (age >= 65) {
    ageMinMaj = 'maggiorenne over 65';
    toSubtract = Math.round((priceTotal / 100) * discount40);
    discountUsed = discount40;
 }
 
+//PRINT HTML
 document.getElementById('majority').innerHTML = ageMinMaj;
 
 document.getElementById('number-discount-percentage').innerHTML = discountUsed;
