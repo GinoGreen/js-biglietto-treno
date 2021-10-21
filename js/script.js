a9439945726376b7f38cf032cb5160fe4e39298b
GinoGreen <luigiverde97@gmail.com>
2021-10-21 16:09:34 +0200

    commit routine 3 - max due numeri decimali

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ va applicato uno sconto del 40% per gli over 65.
 L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). */
 
 //Dichiarazione variabili
-const kiloMeters = 24;
-const age = 19;
+const kiloMeters = parseFloat(prompt('Inserisci i kilometri da percorrere'));
+const age = parseInt(prompt('Inserisci la tua etá'));
 const pricePerKm = 0.21;
 let priceTotal = pricePerKm * kiloMeters;
 let toSubtract = 0;
@@ -18,12 +18,16 @@ const discount40 = 40;
 if (age < 18) {
    toSubtract = (priceTotal / 100) * discount20;
 } else if (age >= 65) {
-   toSubtract = (priceTotal / 100) * discount40;
+   toSubtract = Math.round((priceTotal / 100) * discount40);
 }
 
-document.getElementById('price-total').innerHTML = priceTotal - toSubtract;
+const priceFinal = (priceTotal - toSubtract).toFixed(2);
 
+document.getElementById('price-total').innerHTML = priceFinal;
+
+//console log
 console.log('kilometri', kiloMeters);
 console.log('etá', age);
-console.log('Sconto da sottrarre al prezzo totale', toSubtract);
 console.log('Prezzo totale', priceTotal);
+console.log('Sconto da sottrarre al prezzo totale', toSubtract);
+console.log('Prezzo finale', priceFinal);
