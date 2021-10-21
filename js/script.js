669fac9a518a6dcde6949190124b88136100458f
GinoGreen <luigiverde97@gmail.com>
2021-10-21 18:14:37 +0200

    esercitazione completata

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ L’output del prezzo finale va messo fuori in forma umana (con massimo due deci
 const kiloMeters = parseFloat(prompt('Inserisci i kilometri da percorrere'));
 const age = parseInt(prompt('Inserisci la tua etá'));
 const codeDiscount = prompt('Inserisci il codice sconto, se ne hai uno');
+let codeDiscountAccepted = 0
 const pricePerKm = 0.21;
 let priceTotal = pricePerKm * kiloMeters;
 let toSubtract = 0;
@@ -16,66 +17,47 @@ const discount20 = 20;
 const discount40 = 40;
 let ageMinMaj = 'maggiorenne under 65';
 let discountUsed = 0;
+let stringCodeDiscount = '';
 
 //Inizio esercizio
-
-
 if (age < 18) {
    ageMinMaj = 'minorenne';
    discountUsed = discount20;
-
-   if (codeDiscount === 'SCONTO20') {
-      toSubtract = ((priceTotal / 100) * discount20) * 2 ;
-      document.getElementById('double-discount').innerHTML = 
-      `
-         Il tuo codice sconto é stato applicato correttamente, hai ricevuto un doppio sconto del 20 %
-      `;
-   } else {
-      toSubtract = (priceTotal / 100) * discount20;
-      document.getElementById('double-discount').innerHTML = 
-      `
-         Hai inserito un codice sconto non valido
-      `;
-   }
-
-} else if (age <= 20) {
-   
-   if (codeDiscount === 'SCONTO20') {
-      toSubtract = ((priceTotal / 100) * discount20) ;
-      document.getElementById('double-discount').innerHTML = 
-      `
-         Il tuo codice sconto é stato applicato correttamente, hai ricevuto uno sconto del 20 %
-      `;
-   } else {
-      document.getElementById('double-discount').innerHTML = 
-      `
-         Hai inserito un codice sconto non valido
-      `;
-   }
-
-} else if (age > 20) {
-   
-   if (codeDiscount === 'SCONTO20') {
-      document.getElementById('double-discount').innerHTML = 
-      `
-         Il codice sconto da te inserito non é applicabile per la tua fascia di etá
-      `;
-   } else {
-      document.getElementById('double-discount').innerHTML = 
-      `
-         Hai inserito un codice sconto non valido
-      `;
-   }
 } else if (age >= 65) {
    ageMinMaj = 'maggiorenne over 65';
-   toSubtract = Math.round((priceTotal / 100) * discount40);
    discountUsed = discount40;
+}
+
+if (codeDiscount === 'SCONTO20') {
+   if (age < 18) {
+      codeDiscountAccepted = 2;
+      stringCodeDiscount = 'é valido ed é stato aggiunto, il tuo sconto é raddoppiato'
+   } else if (age <= 20) {
+      codeDiscountAccepted = 1;
+      stringCodeDiscount = 'é valido ed é stato aggiunto'
+   } else {
+      stringCodeDiscount = 'non é applicabile perché hai piú di 20 anni'
+   }
+} else {
+   stringCodeDiscount = 'non é valido'
+}
+
+if (codeDiscountAccepted !== 0) {
+   toSubtract = ((priceTotal / 100) * discount20) * codeDiscountAccepted;
+
+} else {
+   toSubtract = ((priceTotal / 100) * discount20);
 }
 
 //PRINT HTML
 document.getElementById('majority').innerHTML = ageMinMaj;
 
 document.getElementById('number-discount-percentage').innerHTML = discountUsed;
+
+document.getElementById('double-discount').innerHTML = 
+`
+   Il tuo codice sconto del 20% ${stringCodeDiscount}
+`;
 
 const priceFinal = (priceTotal - toSubtract).toFixed(2);
 
@@ -87,3 +69,23 @@ console.log('etá', age);
 console.log('Prezzo totale', priceTotal);
 console.log('Sconto da sottrarre al prezzo totale', toSubtract);
 console.log('Prezzo finale', priceFinal);
+
+
+
+
+
+
+
+// let priceTotal = pricePerKm * kiloMeters;
+
+// if (age < 18) {
+//    //aggiorno pricetotal con lo sconto
+// } else if (age >= 65) {
+//    //aggiorno pricetotal con lo sconto
+// }
+
+// if (codeDiscount === 'SCONTO20') {
+      
+// } else {
+
+// }
